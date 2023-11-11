6cf8456d140ed75e0d06b8f1b9b2405d04a24e85
ferty460 <rostovcevvladimir460@gmail.com>
2023-11-11 19:05:03 +0300

    profile

--- profile.php
+++ profile.php
@@ -16,7 +16,54 @@
 
     <main class="main">
         <section class="task__section">
-            
+            <div class="flex">
+                <div class="profile">
+                    <img src="assets/images/profile.svg" alt="profile" class="profile-img">
+                    <div class="user-details">
+                        <h4 class="username">username</h4>
+                        <h5 class="email">user@email.com</h5>
+                    </div>
+                </div>
+                <div class="tasks">
+                    <div class="task task__high">
+                        <div class="details__task">
+                            <h4 class="theme__task">Тема</h4>
+                            <p class="description__task">Задача не выполнена!</p>
+                        </div>
+                        <div>
+                            <p class="date__task">03.11.2023</p>
+                        </div>
+                    </div>
+                    <div class="task task__normal">
+                        <div class="details__task">
+                            <h4 class="theme__task">Тема</h4>
+                            <p class="description__task">Задача не выполнена!</p>
+                        </div>
+                        <div>
+                            <p class="date__task">03.11.2023</p>
+                        </div>
+                    </div>
+                    <div class="task task__low">
+                        <div class="details__task">
+                            <h4 class="theme__task">Тема</h4>
+                            <p class="description__task">Задача не выполнена!</p>
+                        </div>
+                        <div>
+                            <p class="date__task">03.11.2023</p>
+                        </div>
+                    </div>
+                    <div class="task task__done">
+                        <div class="details__task">
+                            <h4 class="theme__task">Тема</h4>
+                            <p class="description__task">Задача выполнена!</p>
+                        </div>
+                        <div>
+                            <p class="date__task">03.11.2023</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </section>
     </main>
 </body>
