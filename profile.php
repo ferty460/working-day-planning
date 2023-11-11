c59f458c2383011176b6e84364bb9bec077547d6
ferty460 <rostovcevvladimir460@gmail.com>
2023-11-11 19:50:12 +0300

    login

--- profile.php
+++ profile.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link rel="stylesheet" href="assets/css/reset.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/media.css">
     <link rel="stylesheet" href="assets/css/header.css">
+    <title>Профиль | Worky✔</title>
 </head>
 
 <body>
@@ -63,7 +63,6 @@
                     </div>
                 </div>
             </div>
-
         </section>
     </main>
 </body>
